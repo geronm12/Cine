a3d7d7eaf3c8842c9dd1aa987b404a3a33e5e480
Geronimo <gerolpz01@gmail.com>
2020-02-17 15:02:07 -0300

    Agregada la inyeccion de dependencias

---
+++ IoC/Inyeccion.cs
@@ -0,0 +1,72 @@
+﻿using Cine.Dominio._4._1_Entidades;
+using Cine.Implementacion.Cine;
+using Cine.Implementacion.ConsultasServicio;
+using Cine.Implementacion.Cronograma;
+using Cine.Implementacion.Dia;
+using Cine.Implementacion.Entrada;
+using Cine.Implementacion.Funcion;
+using Cine.Implementacion.Horario;
+using Cine.Implementacion.Pelicula;
+using Cine.Implementacion.Sala;
+using Cine.Implementacion.Usuario;
+using Cine.Infraestructura;
+using Cine.Infraestructura.Encriptador;
+using Cine.Infraestructura.Repositorio;
+using Cine.Interfaces.Cine;
+using Cine.Interfaces.ConsultasDto;
+using Cine.Interfaces.ConsultasDto.Interfaces;
+using Cine.Interfaces.Cronograma;
+using Cine.Interfaces.Dia;
+using Cine.Interfaces.Entrada;
+using Cine.Interfaces.Funcion;
+using Cine.Interfaces.Horario;
+using Cine.Interfaces.Pelicula;
+using Cine.Interfaces.Repositorio;
+using Cine.Interfaces.Sala;
+using Cine.Interfaces.Usuario;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IoC
+{
+    public static class Inyeccion <T> where T : EntityBase
+    {
+
+        public static void ConfigurationServices(IServiceCollection services)
+        {
+            services.AddDbContext<Datacontext>();
+
+            services.AddTransient<ICineRepository, CineRepository>();
+
+            services.AddTransient<ICronogramaRepository, CronogramaRepository>();
+
+            services.AddTransient<IPeliculaRepository, PeliculaRepository>();
+
+            services.AddTransient<IHorarioRepository, HorarioRepository>();
+
+            services.AddTransient<ISalaRepository, SalaRepository>();
+
+            services.AddTransient<IEntradaRepository, EntradaRepository>();
+
+            services.AddTransient<IDiaRepository, DiaRepository>();
+
+            services.AddTransient<IFuncionRepository, FuncionRepository>();
+
+            services.AddTransient<IUsuarioRepository, UsuarioRepositorio>();
+
+            services.AddTransient<IEncryptar, Encripter>();
+
+            services.AddTransient<IFuncionConsultaServicio, FuncionConsultaServicio>();
+
+            services.AddTransient<ICronogramaConsultaServicio, CronogramaConsultaServicio>();
+
+            services.AddTransient<ICineSalaServicio, CineSalaConsultaServicio>();
+            
+            services.AddTransient<IRepository<T>, Repository<T>>();
+
+        }
+
+    }
+}
